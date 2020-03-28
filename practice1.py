122da9afc3e062f8cc728f75e76caaec9bedc96d
arg <anna.gaukel@gmail.com>
2020-03-28 14:19:18 -0500

    print string

--- practice1.py
+++ practice1.py
@@ -1,3 +1,6 @@
-print('hello world')
-print(150-50)
-print("Hello World")
+# print('hello world')
+# print(150-50)
+# print("Hello World")
+mystring = "Hello World"
+print(mystring)
+print(mystring[-3])
